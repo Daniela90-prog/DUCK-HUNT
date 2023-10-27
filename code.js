a6d918d84f4e407626f5042532822ee7af55dd9b
“Ruben <rubencallejast@gmail.com>
2023-10-26 23:12:29 -0500

    funcionalidad del sonido

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
+const audioElement = document.getElementById('audioElement');
+const toggleSoundButton = document.getElementById('toggleSound');
+
+let isSoundOn = true;
+
+toggleSoundButton.addEventListener('click', () => {
+  if (isSoundOn) {
+    audioElement.pause();
+    toggleSoundButton.textContent = 'Encender Sonido';
+  } else {
+    audioElement.play();
+    toggleSoundButton.textContent = 'Apagar Sonido';
+  }
+  
+  isSoundOn = !isSoundOn;
+});
+
 function addPlayer() {
     var playerName = document.getElementById("playerName").value;
 
